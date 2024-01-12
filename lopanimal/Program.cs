1cec211ef83972e6c9eabfb3edafc0ae18fefcf0
kin280 <74567462+kin280@users.noreply.github.com>
2024-01-12 21:03:30 +0700

    Add project files.

---
+++ lopanimal/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+public abstract class Animal
+{
+    public abstract string MakeSound();
+}
+
+public class Apple : Fruit
+{
+    public override string HowToEat()
+    {
+        return "Apple could be slided";
+    }
+}
+
+public class Chicken : Animal, Edible
+{
+    public override string MakeSound()
+    {
+        return "Chicken: cluck-cluck!";
+    }
+
+    public string HowToEat()
+    {
+        return "could be fried";
+    }
+}
+public interface Edible
+{
+    public string HowToEat();
+}
+
+public abstract class Fruit : Edible
+{
+    public abstract string HowToEat();
+}
+
+public class Orange : Fruit
+{
+    public override string HowToEat()
+    {
+        return "Orange could be juiced";
+    }
+}
+
+public class Tiger : Animal
+{
+    public override string MakeSound()
+    {
+        return "Tiger: roarrrrr!";
+    }
+}
+
+
+
+class AbstractAndInterfaceTests
+{
+  static void Main(string[] args)
+  {
+    Animal[] animals = new Animal[2];
+    animals[0] = new Tiger();
+    animals[1] = new Chicken();
+
+    foreach (Animal animal in animals)
+    {
+      Console.WriteLine(animal.MakeSound());
+
+      if (animal is Chicken)
+      {
+        Edible edibler = (Chicken)animal;
+        Console.WriteLine(edibler.HowToEat());
+      }
+    }
+
+    Fruit[] fruits = new Fruit[2];
+    fruits[0] = new Orange();
+    fruits[1] = new Apple();
+    foreach (Fruit fruit in fruits)
+    {
+      Console.WriteLine(fruit.HowToEat());
+    }
+  }
+}
